e5e1737906b9fc58997ec94115ced535e4171b5d
Yoshiaki Iinuma <yiinuma@hawaii.edu>
2016-08-11 09:37:36 -1000

    Improved update log message

--- lib/user_synchronizer/base.rb
+++ lib/user_synchronizer/base.rb
@@ -186,6 +186,18 @@ module UserSynchronizer
       user_to_s(r) + sprintf(" %s, %s", r['firstName'], r['lastName'])
     end
 
+    def diff_user(a, b)
+      r = [] 
+      r << 'username' unless a['username'] == b['username']
+      r << 'schoolId' unless a['schoolId'] == b['schoolId']
+      r << 'email' unless a['email'].downcase == b['email'].downcase
+      r << 'firstName' unless a['firstName'].strip == b['firstName'].strip
+      r << 'lastName' unless a['lastName'].strip == b['lastName'].strip
+      #r << 'name' unless a['name'].strip == b['name'].strip
+      r << 'role' unless a['role'] == b['role']
+      r.map { |i| "[#{i.upcase}] #{a[i]} > #{b[i]}" }.join(', ')
+    end
+
     # Ignore leading and trailing spaces of name; ??? core removes them ???
     # Ignore case of email; ??? core make it all lowercase ???
     def compare_user(a, b)
@@ -266,7 +278,7 @@ module UserSynchronizer
     end
 
     def core_update_user(cur_user, updated_user, dry = false)
-      logger.info "UPD: #{user_to_s(cur_user)} => #{user_to_s(updated_user)}"
+      logger.info "UPD: #{user_to_s_long(cur_user)} => #{user_to_s_long(updated_user)} DIFF: #{diff_user(cur_user, updated_user)}"
       if dry
         increment_updated
         return true 
